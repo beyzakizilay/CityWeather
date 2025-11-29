3dd7cc7db177bbc1829305ebb7da0b8fa0e4b74b
beyzakizilay <beyzakizilay4@hotmail.com>
2025-11-29 12:32:58 +0300

    .

--- CityWeather/sehir/views.py
+++ CityWeather/sehir/views.py
@@ -23,3 +23,63 @@ def favoriler(request):
 
 def hakkinda(request):
     return render(request, 'sehir/hakkinda.html')
+from django.shortcuts import render
+
+def sehir_detay(request, sehir_adi):
+    sehirler = {
+        'Izmir': {
+            'name': 'İzmir',
+            'main': {'temp': 22},
+            'weather': [{'description': 'Güneşli'}],
+        },
+        'Istanbul': {
+            'name': 'İstanbul',
+            'main': {'temp': 18},
+            'weather': [{'description': 'Bulutlu'}],
+        },
+        'Ankara': {
+            'name': 'Ankara',
+            'main': {'temp': 15},
+            'weather': [{'description': 'Yağmurlu'}],
+        },
+        'Bursa': {
+            'name': 'Bursa',
+            'main': {'temp': 20},
+            'weather': [{'description': 'Parçalı Bulutlu'}],
+        },
+        'Antalya': {
+            'name': 'Antalya',
+            'main': {'temp': 25},
+            'weather': [{'description': 'Güneşli'}],
+        },
+        'Adana': {
+            'name': 'Adana',
+            'main': {'temp': 28},
+            'weather': [{'description': 'Sıcak'}],
+        },
+        'Trabzon': {
+            'name': 'Trabzon',
+            'main': {'temp': 16},
+            'weather': [{'description': 'Yağmurlu'}],
+        },
+        'Konya': {
+            'name': 'Konya',
+            'main': {'temp': 14},
+            'weather': [{'description': 'Rüzgarlı'}],
+        },
+        'Mersin': {
+            'name': 'Mersin',
+            'main': {'temp': 27},
+            'weather': [{'description': 'Güneşli'}],
+        },
+        'Eskişehir': {
+            'name': 'Eskişehir',
+            'main': {'temp': 17},
+            'weather': [{'description': 'Bulutlu'}],
+        },
+    }
+
+    hava = sehirler.get(sehir_adi)
+
+    return render(request, 'sehir/sehir_detay.html', {'hava': hava})
+
