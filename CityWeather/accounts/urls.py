46ca32aa93ebc51021288a02ecf207d447b9339d
beyzakizilay <beyzakizilay4@hotmail.com>
2025-11-29 16:22:15 +0300

    .

--- CityWeather/accounts/urls.py
+++ CityWeather/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register
+from . import views
 
 urlpatterns = [
-    path("register/", register, name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
 ]
